1feb90d2e8ede072ac4f49bf177551163bbfefcc
olivier coudray <olivier.coudray@stellantis.com>
2022-10-12 11:51:51 +0200

    Remove useless import

--- pysarpu/__init__.py
+++ pysarpu/__init__.py
@@ -5,3 +5,5 @@ __email__ = 'olivier.coudray.15@polytechnique.org'
 __version__ = '0.1.0'
 
 from .pysarpu import PUClassifier
+# from .classification import LinearLogisticRegression, LinearDiscriminantClassifier
+# from .propensity import LogisticPropensity, LogProbitPropensity, GumbelPropensity
